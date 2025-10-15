7bf708934ee74f9176a6eeefae18634ccd0f29ac
Abhishek Kumar <abhisheksingg363@gmail.com>
2025-10-15 11:03:18 +0530

    latest change

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -3,9 +3,9 @@ const { createClient }  = require('redis');
 const redisClient= createClient({
     username: 'default',
     password: process.env.REDIS_PASS,
-  socket: {
-        host: 'redis-13064.crce182.ap-south-1-1.ec2.redns.redis-cloud.com',
-        port: 13064
+     socket: {
+        host: 'redis-14633.c17.us-east-1-4.ec2.redns.redis-cloud.com',
+        port: 14633
     }
 });
 module.exports=redisClient
